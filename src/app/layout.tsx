b1fbac7fc5dcb7c867b320a1e36ee13428c4a340
Jeff <jeffrey.border@gmail.com>
2024-09-30 10:50:57 -0400

    Updated  better screenshot images and added metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,12 @@
 import '@/app/ui/global.css';
 import { montserrat, figtree } from '@/app/ui/fonts';
 import { GoogleAnalytics } from '@next/third-parties/google'
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Jeff Border's Portfolio",
+  description: "Jeff's portfolio of UX Design projects",
+};
  
 export default function RootLayout({
   children,
